74d9a3ed7347e912f687700acf436cadfff9c1a7
midCenturyDude7 <mgriffes@gmail.com>
2022-02-08 12:17:19 -0800

    Building with React components

--- react-social-media-app/src/index.js
+++ react-social-media-app/src/index.js
@@ -1,17 +1,20 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom'
+
+const Header = (props) => {
+    return <h1>Hello, {props.username}!</h1>;
+};
+
+function Layout(props) {
+    return <div style={{ background: "palegoldenrod" }}>{props.children}</div>;
+}
+
+const rootNode = document.getElementById('root');
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <Layout>
+        <Header username="Matt" />
+        <footer>Copyright 2022</footer>
+    </Layout>,
+    rootNode
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
