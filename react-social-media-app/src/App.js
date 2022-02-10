c9dc4afc274ca2211f9321149b0307dca097090c
midCenturyDude7 <mgriffes@gmail.com>
2022-02-10 13:53:54 -0800

    Creating structure, format for Login component

--- react-social-media-app/src/App.js
+++ react-social-media-app/src/App.js
@@ -2,11 +2,10 @@ import React from "react";
 import Login from './components/Login'
 
 function App() {
-    let user = "";
+    const [user, setUser] = React.useState('')
 
     if (!user) {
-        return <Login />;
-
+        return <Login setUser={setUser} />;
     }
     return <div>app</div>;
 }
