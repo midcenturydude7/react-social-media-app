39850b96106b435a39309155e503b49522eb181d
midCenturyDude7 <mgriffes@gmail.com>
2022-02-09 22:06:22 -0800

    Setting up the basic HTML structure of the Login component

--- react-social-media-app/src/App.js
+++ react-social-media-app/src/App.js
@@ -1,7 +1,13 @@
 import React from "react";
-
+import Login from './components/Login'
 
 function App() {
+    let user = "";
+
+    if (!user) {
+        return <Login />;
+
+    }
     return <div>app</div>;
 }
 
